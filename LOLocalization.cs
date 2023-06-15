ef06912e0ccafac866718e6fcaee5be8c1a07300
Kautism <kautism.nung@accelstor.com>
2023-06-15 14:09:06 +0800

    Fix UI do not translation bugs.

--- LOLocalization.cs
+++ LOLocalization.cs
@@ -22,6 +22,7 @@ namespace LOLocalization
         internal new static ManualLogSource Log;
         public static int langindex = 4;
         public static string langtext = "tc";
+        public static string[] innerTextAssets = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "40", "41", "57", "155", "168", "600", "601", "602", "603", "604", "605", "625", "626", "806", "807", "810", "814", "828", "831", "833", "836", "839", "840", "847", "848", "869", "894", "895", "1181", "1184", "1185", "1186", "1187", "1188", "1189", "1190", "1223", "1285", "1286", "1338", "1342", "1343", "1344", "1348", "1352", "1371", "1374", "1375", "1376", "1410", "1440", "1441", "8000", "8001", "8002", "8003", "8004", "8005", "8006" };
 
         public static System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, string>> communityLocalizationPatch = null;
         public static System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, string>> communityTable_LocalizationPatch = null;
@@ -124,7 +125,6 @@ namespace LOLocalization
             if (name == "Table_Localization_ja")
                 name = "Table_Localization_" + langtext;
         }*/
-        /*
         [HarmonyPrefix, HarmonyPatch(typeof(Localization), nameof(Localization.AddCSV))]
         static void AddCSVPatch(ref BetterList<string> newValues, Il2CppStringArray newLanguages, Dictionary<string, int> languageIndices)
         {
@@ -137,13 +137,16 @@ namespace LOLocalization
                 for (int i = 0; i < newLanguages.Length; i++)
                     sb.Append(newLanguages[i]);
             }
-            Log.LogDebug($"AddCSV({ newValues.size}): {sb}");
-            if (newValues.size >= 6 && newValues[langindex] != null && newValues[langindex].Trim().Length != 0)
+            int local_langindex = langindex;
+            if (innerTextAssets.Contains(newValues[0]))
+                local_langindex = 5;
+            //Log.LogInfo($"AddCSV({ newValues.size})[{newValues[0]}]: {sb}");
+            if (newValues.size >= 6 && newValues[local_langindex] != null && newValues[local_langindex].Trim().Length != 0)
             {
-                newValues[1] = newValues[langindex];
-                newValues[2] = newValues[langindex];
+                newValues[1] = newValues[local_langindex];
+                newValues[2] = newValues[local_langindex];
             }
-        }*/
+        }
 
 
         [HarmonyPrefix, HarmonyPatch(typeof(Localization), nameof(Localization.Get))]
